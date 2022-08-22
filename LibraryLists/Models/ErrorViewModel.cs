18e7e812e971aa9e8412f682ebc30c0e8d32fd62
Hayden <haydenbruceanderson12@gmail.com>
2022-08-22 07:37:12 -0700

    Add project files.

---
+++ LibraryLists/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryLists.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
